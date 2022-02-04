3591fa4ebb992b8543166bff4c661f71cf3919b0
Poyraz <poyraztcc@gmail.com>
2022-02-03 23:04:16 -1000

    visualizer waves working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import Sketch from 'react-p5';
 import 'p5/lib/addons/p5.sound';
-import Beat from '../../Beat.mp3';
+import Beat from '../../lights.mp3';
 
 const App = () => {
 
-  let width = 700;
-  let height = 400;
+  let width = 1000;
+  let height = 700;
   let song;
+  let fft;
 
-  const preload = (p5) => {
-    p5.soundFormats('mp3');
-    song = p5.loadSound(Beat);
+  const preload = (p) => {
+    p.soundFormats('mp3');
+    song = p.loadSound(Beat);
   }
 
-  const setup = (p5, canvasParentRef) => {
+  const setup = (p, canvasParentRef) => {
+    // react-p5 conveniently initializes window.p5 thats why all the other p5's
+    // have been changed to p in order to create an FFT object
+    fft = new p5.FFT();
+    // console.log(p.point)
     // use parent to render the canvas in this ref
 		// without it p5 will render the canvas outside of this component
-    const canvas = p5.createCanvas(width, height).parent(canvasParentRef);
+    const canvas = p.createCanvas(width, height).parent(canvasParentRef);
     canvas.mousePressed(() => {
       if (song.isPlaying()) {
         song.pause();
@@ -27,9 +32,27 @@ const App = () => {
     });
   }
 
-  const draw = p5 => {
-    p5.background(0);
-    p5.ellipse(350, 200, 100)
+  const draw = p => {
+    p.background(0);
+    // by default the stroke color is black
+    // we need to change this in order to see the wave
+    p.stroke(255, 204, 0);
+
+    // returns an array with 1024 elements
+    let wave = fft.waveform();
+
+    // By looping through the waveform data, we are able
+    // to draw the waveform across the canvas
+    for (let i = 0; i < width; i++) {
+      // create an index that maps the for loop variable
+      // to the index of the wave we want
+      // value must be intereger thats we we use floor
+      let index = p.floor(p.map(i, 0, width, 0, wave.length));
+
+      let x = i;
+      let y = wave[index] * 180 + height / 2;
+      p.point(x, y);
+    }
   }
 
   return (
